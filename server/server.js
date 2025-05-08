e946ecbbca236f25907c6185940a2b35cd6e5495
vijaykumarmganj1 <vijaykumarmganj1@gmail.com>
2025-05-08 10:18:15 +0530

    making a real time chat app using socket.io today i start creating

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from MERN stack!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
+
+
